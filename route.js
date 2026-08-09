8b0c38d5c7a0cfeba972427211857aa09e1fcf03
Pranto Ghosh <prantoas8002@gmail.com>
2026-08-09 21:07:19 +0600

    limiter apply

--- route.js
+++ route.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
-const {getBlog, createBlog, registerUser, loginUser} = require("./controller");
+const { getBlog, createBlog, registerUser, loginUser } = require("./controller");
 const { checkUser } = require('./middleware');
+const { skip } = require('node:test');
 
 const router = express.Router();
 
-router.get('/blogs' , getBlog);
-router.post('/blogs' ,checkUser, createBlog);
-router.post('/register' , registerUser);
-router.post('/login' , loginUser);
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 requests per windowMs
+    skipSuccessfulRequests: true, // Skip counting successful requests
+    message: { status: "fail"  , message: 'Too many requests, please try again later.' }
+});
+
+router.get('/blogs', getBlog);
+router.post('/blogs', checkUser, createBlog);
+router.post('/register', authLimiter, registerUser);
+router.post('/login', authLimiter, loginUser);
 
 
 module.exports = router;
